e1542d02f1123c3c17d12177023b581275b970ea
InKyuHwang001 <hwanginkyu42@naver.com>
2024-11-17 16:18:31 +0900

    "11/17"

--- src/main/java/org/dataStructure/day_1/sol1439/Main2.java
+++ src/main/java/org/dataStructure/day_1/sol1439/Main2.java
@@ -1,4 +1,27 @@
 package org.dataStructure.day_1.sol1439;
 
+import java.io.*;
+
 public class Main2 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+
+        String request = br.readLine();
+
+        String[] req = request.split("");
+
+        String now = req[0];
+        int ans = 1;
+
+        for (String a : req){
+            if (!now.equals(a)){
+                ans += 1;
+                now = a;
+            }
+        }
+
+        System.out.println((ans/2));
+    }
 }
